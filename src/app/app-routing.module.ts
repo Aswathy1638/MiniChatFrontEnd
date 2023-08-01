1654cab232fe4dd661137d7bc57c19c5c1c3d735
Aswathy1638 <aswathy1638@gmail.com>
2023-08-01 15:19:14 +0530

    Changed login and register CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {LoginComponent} from './login/login.component';
 import {UserlistComponent} from './userlist/userlist.component';
 import {ConversationhistoryComponent} from './conversationhistory/conversationhistory.component'
 import {RequestLogsComponent}from './request-logs/request-logs.component'
+import {CommonComponent} from './common/common.component'
 const routes: Routes = [
   {path:'',component:LoginComponent},
   {path:'register',component: UserRegisterComponent},
  {path:'chat/user/:userId',component:ConversationhistoryComponent},
   {path:'chat',component:UserlistComponent},
-  {path:'logs',component:RequestLogsComponent}
+  {path:'logs',component:RequestLogsComponent},
+  {path:'app-common',component:CommonComponent}
   
 ];
 
